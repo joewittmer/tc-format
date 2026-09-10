099dd764802148e973e7915db78c7ecdb369f361
Joe Wittmer <joseph.e.wittmer@gmail.com>
2026-09-10 11:29:10 -0500

    release version 1.0.7.0

--- installer/build.ps1
+++ installer/build.ps1
@@ -2,7 +2,7 @@
 param(
     [Parameter()]
     [ValidatePattern('^\d+\.\d+\.\d+(?:\.\d+)?(?:-[0-9A-Za-z.-]+)?(?:\+[0-9A-Za-z.-]+)?$')]
-    [string] $Version = '1.0.6.0',
+    [string] $Version = '1.0.7.0',
 
     [Parameter()]
     [string] $PublishDirectory,
